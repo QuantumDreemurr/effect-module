576f8f7eb64446da83e451bacc6d605e1a301bf3
scandalous <80922445+QuantumDreemurr@users.noreply.github.com>
2023-01-17 18:24:30 +0000

    Create doc.lua

---
+++ doc.lua
@@ -0,0 +1,48 @@
+-- DOCUMENTATION
+
+--[[
+	[Particle]
+		
+		Size
+			type Vector3
+			default 1, 1, 1
+			
+		GoalSize
+			type Vector3
+			default Size * 2
+		
+		Time
+			type number
+			default 0.45
+		
+		Position
+			type Vector3
+		
+		GoalPosition
+			type Vector3
+			default Position
+		
+		Transparency
+			type number
+			default 0.25
+		
+		GoalTransparency
+			type number
+			default 1
+		
+		Rotation
+			type Vector3
+			default 0, 0, 0
+			
+		GoalRotation
+			type Vector3
+			default 0, 0, 0
+			
+		Shape
+			type Enum.PartType OR string
+			default Ball
+		
+		Acceleration
+			type Vector3
+			defualt 0, 0, 0
+]]
